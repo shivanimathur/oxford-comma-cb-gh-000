39ae08389268199b92a1b80caba771255356f1e3
Shivani Mathur <qwellery@gmail.com>
2017-12-27 13:43:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,11 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   elsif array.size == 3
+    array[-1] = " and #{array[-1]}"
+    array.join(, )
+    
+  elsif array.size > 3
     array.join(,)
-
-
+    
   end
 end
