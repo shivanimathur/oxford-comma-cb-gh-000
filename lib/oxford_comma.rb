dcab0837a34a895eb3a496f04fd36db611584e54
Shivani Mathur <qwellery@gmail.com>
2017-12-27 14:05:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.size == 2
     return array.join(" and ")
-  elsif array.size == 3
+  elsif array.size >= 3
     size = array.size
     index = (0..size-2).to_a
     index.each do |index|
@@ -11,14 +11,5 @@ def oxford_comma(array)
     end
     array[size-1] = "and #{array[size-1]}"
     return array.join
-  elsif array.size > 3
-    size = array.size
-    index = (0..size-2).to_a
-    index.each do |index|
-      array[index] = "#{array[index]}, "
-    end
-    array[size-1] = "and #{array[size-1]}"
-    return array.join
-
   end
 end
